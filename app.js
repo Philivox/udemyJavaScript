d3ccd99fcefc4d16e065fede28b7550b9c68894f
Andy Tran <andy.tran.88@gmail.com>
2018-10-17 16:35:54 -0500

    initial commit

---
+++ app.js
@@ -0,0 +1,14 @@
+//var, let, and const are all keywords in javascript
+//get liveserver, JS code snippets, bracket pair colorizer, and change word wrap to "enabled"
+var name = 'John Doe';
+console.log(name);
+name = 'Steve Smith';
+console.log(name);
+
+//Init is used for initializing a variable
+
+var greeting;
+console.log(greeting);
+//This will result in it saying "undefined"
+greeting = "hello";
+console.log(greeting);
