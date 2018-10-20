b2b557f111756879e92d628390161726cfb91fb4
Philivox <andy.tran.88@gmail.com>
2018-10-19 23:13:44 -0500

    added object literals file

--- arrays.js
+++ arrays.js
@@ -14,18 +14,31 @@ numbers[2] = 100;
 
 //--Array Mutation--
 
-//Pushing to an array, adding onto the end
-numbers.push(250);
-//Adding to the front
-numbers.unshift(1032);
-//Remove from the end
-numbers.pop();
-//Remove from front
-numbers.shift();
-//Splice values, first number is where you want to start, and second number is where you want to end
-numbers.splice(1,3);
-//Reverse the array
-numbers.reverse();
-//Concatenate an array
-val = numbers.concat(overwatch);
+// //Pushing to an array, adding onto the end
+// numbers.push(250);
+// //Adding to the front
+// numbers.unshift(1032);
+// //Remove from the end
+// numbers.pop();
+// //Remove from front
+// numbers.shift();
+// //Splice values, first number is where you want to start, and second number is where you want to end
+// numbers.splice(1,3);
+// //Reverse the array
+// numbers.reverse();
+// //Concatenate an array
+// val = numbers.concat(overwatch);
+
+val = numbers.sort();
+//Won't give you a sort of ascending or descending, it's just a sorting of the numbers in array order. You want to use the compare function
+val = numbers.sort(function(x,y){
+    return x-y;
+});
+
+//FIND
+function under50(num){
+    return num < 50;
+}
+val = numbers.find(under50);
+//Just returns the first number under 50
 console.log(val);
---
+++ object_literals.js
@@ -0,0 +1,7 @@
+const person = {
+    firstName: 'Andy'
+}
+
+let val;
+val = person;
+console.log(val);
