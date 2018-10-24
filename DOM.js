3504aed34c79142120903c682ee9113af267d2a0
Philivox <andy.tran.88@gmail.com>
2018-10-23 21:39:34 -0500

    Added multiple query selector and DOM traversal to DOM.js

--- DOM.js
+++ DOM.js
@@ -50,15 +50,74 @@
 
 //--MULTIPLE ELEMENT SELECTORS--
 
-//Getting Elements by ClassName
-const items = document.getElementsByClassName('collection-item');
-//Using this you can get all the information for each item arranged in a list
+// //Getting Elements by ClassName
+// const items = document.getElementsByClassName('collection-item');
+// //Using this you can get all the information for each item arranged in a list
 
-items[3].style.color = 'red';
-//Just like an array you can change certain properties of the list item
-items[0].textContent = 'Hello';
+// items[3].style.color = 'red';
+// //Just like an array you can change certain properties of the list item
+// items[0].textContent = 'Hello';
 
-//Using getElements on a global scope
-const listItems = document.querySelector('ul').getElementsByClassName('collection-item');
+// //Using getElements on a global scope
+// const listItems = document.querySelector('ul').getElementsByClassName('collection-item');
 
-console.log(listItems);
+// //Getting elements by tag name
+// let lis = document.getElementsByTagName('li');
+// //Printing this will give you an HTML collection with all the "li" elements in the doc
+// console.log(lis[0]);
+// //Again, you can return this collection like an array
+// lis[0].style.color = 'red';
+// lis[3].textContent = 'World';
+// //Like other selectors you can change the text content and style elements
+
+// //Converting HTML Collection into Array
+// lis = Array.from(lis);
+// //Don't forget to change "const lis = ..." into "let lis = ..."
+// lis.reverse();
+
+// lis.forEach(function(li, index){
+//   console.log(li.className)
+//   // li.textContent = 'Oh No!';
+//   li.textContent = `${index}: Awww yeah`;
+// });
+
+//Query Multi-Selectors
+
+// const items = document.querySelectorAll('ul.collection li.collection-item');
+
+// items.forEach(function(item, index){
+//   item.textContent = `${index}: New Node Item`
+// })
+
+// //Affecting the odd/even numbered items with node collection
+// const liOdd = document.querySelectorAll('li:nth-child(odd)');
+// const liEven = document.querySelectorAll('li:nth-child(even)');
+
+// liOdd.forEach(function (li, index) {
+//   li.style.background = '#ccc';
+// });
+// //This will make all the odd item in the list have a grey background
+
+// for(let i = 0; i < liEven.length; i++){
+//   liEven[i].style.background = '#f4f4f4';
+// }
+// //This demonstrates the versatility of the querySelector using a for loop
+
+// console.log(items);
+
+//--TRAVERSING THE DOM--
+
+let val;
+
+const list = document.querySelector('ul.collection');
+const listItem = document.querySelector('li.collection-item:first-child');
+//All items in the table of the page have the class of collection-item so it'll only select the first item, if we left off the "first-child" part it'd do the same thing
+
+val = listItem;
+val = list;
+//Assigning list to val will select the entire table in the console
+
+//Getting child nodes of unordered list
+val = list.childNodes;
+//What you get in the console is a list of the child nodes 
+console.log(val);
